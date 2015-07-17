04dad16846e4e849320731707010a95164ffc1ca
Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
2015-07-18 01:13:27 +0200

    Progress in adapting tabix.

--- pyhtslib/hts_internal.py
+++ pyhtslib/hts_internal.py
@@ -11,16 +11,36 @@ __author__ = 'Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>'
 # export everything from this submodule manually, including the code that
 # starts with an underscore, importing modules will not import the latter
 __all__ = [
+    # constants (through ``#define``)
+    '_HTS_IDX_NOCOOR',
+    '_HTS_IDX_START',
+    '_HTS_IDX_REST',
+    '_HTS_IDX_NONE',
     # htslib types
-    '_hts_idx_t', '_BGZF',
+    '_hts_idx_t',
+    '_BGZF',
     # htslib functions
-    '_hts_itr_destroy', '_hts_itr_next', '_hts_itr_queryi',
+    '_hts_itr_destroy',
+    '_hts_itr_next',
+    '_hts_itr_query',
+    '_hts_itr_queryi',
     '_hts_itr_querys',
+    '_tbx_readrec',
 ]
+
+
+_HTS_IDX_NOCOOR = -2
+_HTS_IDX_START = -3
+_HTS_IDX_REST = -4
+_HTS_IDX_NONE = -5
 
 
 class _hts_idx_t(ctypes.Structure):
     """Generic type for htslib index types"""
+
+
+class _hts_itr_t(ctypes.Structure):
+    """Generic type for htslib iterator types"""
 
 
 class _BGZF(ctypes.Structure):
@@ -34,10 +54,24 @@ _hts_itr_destroy = htslib.itr_destroy
 _hts_itr_destroy.restype = None
 
 _hts_itr_next = htslib.hts_itr_next
-_hts_itr_next.restype = None
+_hts_itr_next.restype = ctypes.c_int
 
-_hts_itr_queryi = htslib.itr_queryi
-_hts_itr_queryi.restype = None
+_hts_itr_query = htslib.itr_queryi
+_hts_itr_query.restype = ctypes.POINTER(_hts_itr_t)
 
-_hts_itr_querys = htslib.itr_querys
-_hts_itr_querys.restype = None
+
+def _hts_itr_queryi(tbx, tid, beg, end):
+    return _hts_itr_query(tbx[0].idx, tid, beg, end, _tbx_readrec)
+
+
+def _hts_itr_querys(tbx, s):
+    return _hts_itr_query(tbx[0].idx, s, _tbx_name2id, tbx, _hts_itr_query,
+                          _tbx_readrec)
+
+
+_tbx_name2id = htslib.tbx_name2id
+_tbx_name2id.restype = ctypes.c_int
+
+
+_tbx_readrec = htslib.tbx_readrec
+_tbx_readrec.restype = ctypes.c_int
--- pyhtslib/tabix.py
+++ pyhtslib/tabix.py
@@ -3,6 +3,8 @@
 
 import os
 
+from tabix_internal import *  # NOQA
+
 __author__ = 'Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>'
 
 
@@ -13,6 +15,12 @@ class TabixIndexException(Exception):
 class TabixConfig:
     """Configuration for tabix-indxed files"""
 
+    @staticmethod
+    def from_c_struct(self, name):
+        res = _tbx_conf_t.in_dll(htslib, name)
+        return TabixConfig(res.preset, res.seq_col, res.begin_col,
+                           res.end_col, res.meta_char, res.line_skip)
+
     def __init__(self, preset, seq_col, begin_col, end_col, meta_char,
                  line_skip):
         self.preset = preset
@@ -21,43 +29,38 @@ class TabixConfig:
         self.end_col = end_col
         self.meta_char = meta_char
 
-
-class TabixGFFConfig(TabixConfig):
-    """Tabix presets for GFF"""
-
-    def __init__(self):
-        pass
-
-
-class TabixBEDConfig(TabixConfig):
-    """Tabix presets for BED"""
-
-    def __init__(self):
-        pass
+    def to_c_struct(self):
+        """Convert to C struct"""
+        res = _tbx_conf_t()
+        res.preset = self.preset
+        res.sc = self.sc
+        res.bc = self.bc
+        res.ec = self.ec
+        res.meta_char = self.meta_char
 
 
-class TabixPSLTBLConfig(TabixConfig):
-    """Tabix presets for PSLTBL
-
-    This is the output of Jim Kent's BLAT, for example.
-    """
-
-    def __init__(self):
-        pass
+TBX_CONF_GFF = TabixConfig.from_c_struct('tbx_conf_gff')
+TBX_CONF_BED = TabixConfig.from_c_struct('tbx_conf_bed')
+TBX_CONF_PLSTBL = TabixConfig.from_c_struct('tbx_conf_plstbl')
+TBX_CONF_SAM = TabixConfig.from_c_struct('tbx_conf_sam')
+TBX_CONF_VCF = TabixConfig.from_c_struct('tbx_conf_vcf')
 
 
-class TabixSAMConfig(TabixConfig):
-    """Tabix presets for SAM"""
+class TabixFileIter:
+    """Allows iteration over tabix files"""
 
-    def __init__(self):
-        pass
+    def __init__(self, index, struct_ptr):
+        self.index = index
+        self.struct_ptr = struct_ptr
+        self.struct = self.struct_pr[0]
 
+    def __iter__(self):
+        return self
 
-class TabixVCFConfig(TabixConfig):
-    """Tabix presets for VCF"""
-
-    def __init__(self):
-        pass
+    def __next__(self):
+        if _tbx_itr_next(htsfp, self.index.struct_ptr, r, data):
+            pass
+        raise StopIteration()
 
 
 class TabixIndex:
@@ -65,7 +68,17 @@ class TabixIndex:
 
     @staticmethod
     def build(path, min_shift, config, tbi_path=None):
-        pass
+        """Build tabix index for file at path
+
+        If ``tbi_path`` is given then the index is built at the given path.
+        Otherwise, it is built at ``${path}.tbi``.
+        """
+        if tbi_path:
+            _tbx_index_build2(path, tbi_path, min_shift,
+                              ctypes.byref(config.to_c_struct_ptr))
+        else:
+            _tbx_index_build2(path, min_shift,
+                              ctypes.byref(config.to_c_struct_ptr))
 
     def __init__(self, path, tbi_path=None, require_index=False,
                  auto_load=True, auto_build=True):
@@ -81,6 +94,11 @@ class TabixIndex:
         #: exist yet, overrides ``require_index``
         self.auto_build = auto_build
 
+        #: wrapped C struct
+        self.struct = None
+        #: pointer to C struct
+        self.struct_ptr = None
+
         self._check_auto_build()
         self._check_auto_load()
         self._check_require_index()
@@ -88,36 +106,60 @@ class TabixIndex:
     def _check_auto_build(self):
         if not self.auto_build:
             return
-        if not os.path.exists(self.fai_path):
-            TabixIndex.build(self.fasta_path)
+        if not os.path.exists(self.tai_path):
+            TabixIndex.build(self.path)
 
     def _check_auto_load(self):
         if not self.auto_load:
             return
-        if os.path.exists(self.fai_path):
+        if os.path.exists(self.tai_path):
             self.load()
         else:
             tpl = 'Tabix index required for {} for loading but not found.'
-            raise TabixIndexException(tpl.format(self.fasta_path))
+            raise TabixIndexException(tpl.format(self.path))
 
     def _check_require_index(self):
         if not self.require_index:
             return
-        if not os.path.exists(self.fai_path):
+        if not os.path.exists(self.tai_path):
             tpl = 'Tabix index required for {} but not found.'
-            raise TabixIndexException(tpl.format(self.fasta_path))
+            raise TabixIndexException(tpl.format(self.path))
 
     def query(self, region_str):
-        pass
+        return TabixFileIter(self, _tbx_itr_querys(self.struct_ptr,
+                                                   region_str))
 
     def query_region(self, seq, begin, end):
-        pass
+        return TabixFileIter(self, _tbx_itr_queryi(
+            self.struct_ptr, seq, begin, end))
+
+    def from_start(self):
+        return TabixFilteIter(self, _tbx_itr_query(self.struct_ptr))
+
+    def from_current(self):
+        return TabixFilteIter(self, _hts_itr_query(
+            self.struct_ptr, _HTS_IDX_REST, 0, 0, _tbx_readrec))
 
     def load(self):
-        pass
+        self.close()
+        if self.tai_path:
+            self.struct_ptr = _tbx_index_load2(self.path, self.tbi_path)
+            if not self.struct_ptr:
+                tpl = 'Could not load tabix index for {}'
+                raise TabixIndexException(tpl.format(self.path))
+        else:
+            self.struct_ptr = _tbx_index_lod(self.path)
+            if not self.struct_ptr:
+                tpl = 'Could not load tabix index {} for {}'
+                raise TabixIndexException(tpl.format(self.tbi_path,
+                                                     self.path))
+        self.struct = self.struct_ptr[0]
 
     def close(self):
-        pass
+        if self.struct_ptr:
+            _tbx_destroy(self.struct_ptr)
+        self.struct = None
+        self.struct_ptr = None
 
     def __enter__(self):
         return self
